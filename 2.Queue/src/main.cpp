41d0e38b6d1495011acea43f7e1db826be496574
TungNT2000 <nguyenthanhtung082k@gmail.com>
2023-07-27 17:35:53 +0700

    add queue example

--- 2.Queue/src/main.cpp
+++ 2.Queue/src/main.cpp
@@ -7,16 +7,34 @@ TaskHandle_t TaskHandle_1;
 TaskHandle_t TaskHandle_2;
 // init Queue
 QueueHandle_t Queue;
-
+QueueHandle_t Queue1;
+struct Messenge
+{
+  int id;
+  char data[20];
+} xMess;
+char TxBuff[30];
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
   // 1 create task
-  xTaskCreate(task1, "task1", 1024 * 2, NULL, 1, &TaskHandle_1);
-  xTaskCreate(task2, "task2", 1024 * 2, NULL, 1, &TaskHandle_2);
+
+  Queue1 = xQueueCreate(7, sizeof(TxBuff));
+  xTaskCreatePinnedToCore(task1, "task1", 1024, NULL, 1, &TaskHandle_1, 1);
+  xTaskCreatePinnedToCore(task2, "task2", 1024, NULL, 1, &TaskHandle_2, 1);
 }
-void loop() {}
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    String buff = Serial.readStringUntil('\n');
+    Serial.print("\nReceived String: ");
+    Serial.println(buff);
+    sprintf(TxBuff, buff.c_str());
+    xQueueSend(Queue, (void *)TxBuff, (TickType_t)0);
+  }
+}
 
 void task1(void *p)
 {
@@ -28,6 +46,7 @@ void task1(void *p)
   xQueueSend(Queue, (void *)&data, (TickType_t)0);
   data = 2;
   xQueueSend(Queue, (void *)&data, (TickType_t)0);
+  ////////////////////////////// Send custom data
   Serial.print("data waiting to be read: ");
   // get queue state
   Serial.print(uxQueueMessagesWaiting(Queue));
@@ -35,6 +54,7 @@ void task1(void *p)
   Serial.print("available spaces: ");
   Serial.print(uxQueueSpacesAvailable(Queue));
   Serial.print("\n");
+
   while (true)
   {
   }
@@ -44,14 +64,24 @@ void task2(void *p)
   vTaskDelay(100 / portTICK_PERIOD_MS);
   Serial.println("on task 2");
   int dataRcv;
+  char RxBuff[30];
   while (true)
   {
     if (Queue != 0)
     {
-      if (xQueueReceive(Queue, (void *)&dataRcv, (TickType_t)5))
+      if (xQueueReceive(Queue, (void *)&dataRcv, (TickType_t)0))
       {
-        Serial.print("data receive: ");
+        Serial.print("data 1 receive: ");
         Serial.print(dataRcv);
+        Serial.print("\n");
+      }
+    }
+    if (Queue1 != 0)
+    {
+      if (xQueueReceive(Queue, (void *)RxBuff, (TickType_t)0))
+      {
+        Serial.print("data 2 receive: ");
+        Serial.print(RxBuff);
         Serial.print("\n");
       }
     }
